89604b2da1d3f42bff2a7f2967d092a010052fb9
Pritom Hazarika <pritomhazarika@Pritoms-MacBook-Air.local>
2026-03-26 01:25:44 +0530

    summury and date bug fix

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
-const CACHE_NAME = 'journal-v1';
+// InterstitialJournal/sw.js
+const CACHE_NAME = 'journal-v2'; // Changed to v2 to force refresh
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './journal_icon.png' // Added your icon to the cache
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
